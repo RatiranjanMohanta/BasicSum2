c081d2c986610a8142273445280ef025ead5c8d6
RatiranjanMohanta <mohanta.ratiranjan1@gmail.com>
2024-10-22 20:21:35 +0530

    1st commit

---
+++ CalculateBasicSum/src/base/CalulateSum.java
@@ -0,0 +1,17 @@
+package base;
+
+public class CalulateSum {
+
+	public int sum(int x,int y) {
+		int sum=x+y;
+		return sum;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CalulateSum obj=new CalulateSum();
+		System.out.println("Result is :"+obj.sum(10, 20));
+
+	}
+
+}
