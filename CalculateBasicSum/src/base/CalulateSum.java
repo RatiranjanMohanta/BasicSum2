807c98c2b0ebf1a032cd8ca9f6fb5a4bc1ff254d
LENOVO <mohanta.ratiranjan1@gmail.com>
2024-10-22 20:41:12 +0530

    comit

--- CalculateBasicSum/src/base/CalulateSum.java
+++ CalculateBasicSum/src/base/CalulateSum.java
@@ -4,7 +4,7 @@ public class CalulateSum {
 
 	public int sum(int x,int y) {
 		int sum=x+y;
-		return sum;
+		return sum; 
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
